f4123e32386dec21b2ef0711dee23c40b6dc6666
tengfei1009 <3240288774@qq.com>
2024-09-09 22:11:51 +0800

    init_2

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,27 +1,39 @@
-import axios from 'axios';
+import request from '@/api/interceptor';
 import type { UserState } from '@/store/modules/user/types';
 
 export interface LoginData {
   username: string;
   password: string;
+  captcha: string;
 }
 
 export interface LoginRes {
   token: string;
 }
+
+export interface RightVerify {
+  code: number;
+  data: {
+    Id: string;
+    Hcode: string;
+    B64: string;
+  };
+  msg: string;
+}
+
 export function login(data: LoginData) {
-  return axios.post<LoginRes>('/api/user/login', data);
+  return request.post<LoginRes>('/api/user/login', data);
 }
 
 export function logout() {
-  return axios.post<LoginRes>('/api/user/logout');
+  return request.post<LoginRes>('/api/user/logout');
 }
 
 export function getUserInfo() {
-  return axios.post<UserState>('/api/user/info');
+  return request.post<UserState>('/api/user/info');
 }
 
-// 获取验证码
-export function getCaptcha(data: { tel: string }) {
-  return axios.post<{ captcha?: string }>('/api/login/captcha', data);
+// 获取图形验证码
+export function getPicCode() {
+  return request.post<RightVerify>('/user/get_verification'); // 这里会自动拼接 baseURL
 }
