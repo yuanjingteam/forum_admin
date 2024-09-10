3225c669e726c5cf1cda8c22c49e7dc3774a0982
tengfei1009 <3240288774@qq.com>
2024-09-10 08:31:09 +0800

    router_change

--- src/router/routes/modules/userManage.ts
+++ src/router/routes/modules/userManage.ts
@@ -9,12 +9,17 @@ const LIST: AppRouteRecordRaw = {
     locale: '用户管理',
     requiresAuth: true,
     icon: 'icon-list',
-    order: 2
+    order: 2,
+    hideChildrenMenu: true // 隐藏子菜单项
   },
+  // 将子路由的 path 设置为空字符串 ''，表示该子路由与父路由的路径完全匹配。
+  // 这意味着访问 / role 时，实际上会加载子路由的组件。
+  // 当用户访问 / role 时，Vue Router 会根据路径找到对应的路由配置，
+  // 渲染 role-maragement / index.vue 组件。
   children: [
     {
-      path: 'search-table', // The midline path complies with SEO specifications
-      name: 'SearchTable',
+      path: '', // 使用空路径以匹配父路由
+      name: 'role', // 子路由名称
       component: () => import('@/views/role-maragement/index.vue'),
       meta: {
         locale: '查询表格',
