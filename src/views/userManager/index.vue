d5a312cf98eb52e65f5c71580135d36a508f90ea
tengfei1009 <3240288774@qq.com>
2024-09-12 22:00:50 +0800

    feat: table_2

--- src/views/userManager/index.vue
+++ src/views/userManager/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { getUserList, getUerItem } from '@/api/user';
-import { TableColumnData } from '@arco-design/web-vue';
-import { ref } from 'vue';
+import { TableColumnData, TableRowSelection } from '@arco-design/web-vue';
+import { ref, reactive } from 'vue';
 const columns: TableColumnData[] = [
   {
     title: '用户ID',
@@ -45,21 +45,38 @@ const columns: TableColumnData[] = [
   },
   {
     title: '最后一次登录',
-    dataIndex: 'last_login_time'
+    dataIndex: 'last_login_time',
+    width: 120
   },
   {
     title: '注册时间',
-    dataIndex: 'create_time'
+    dataIndex: 'create_time',
+    width: 120
   },
   {
     title: '操作',
     dataIndex: 'optional',
     slotName: 'optional',
-    width: 80
+    align: 'center'
   }
 ];
 const userInfo = ref([]);
-const userItem = ref();
+const userItem = ref(null);
+
+const selectedKeys = ref<string[]>([]); // 确保这里初始化为一个空数组
+const pagination = { pageSize: 5 };
+
+const rowSelection: TableRowSelection = reactive({
+  type: 'checkbox',
+  showCheckedAll: true,
+  onlyCurrent: false
+});
+
+// 抽屉是否可见
+const itemVisible = ref(false);
+// 自定义抽屉项
+const itemCustom = ref([]);
+
 const getUsers = async () => {
   const { data } = await getUserList();
   debugger;
@@ -74,38 +91,91 @@ const switchChange = state => {
 
 // 获取当前列
 const editItem = async userId => {
-  const { data } = await getUerItem({ user_id: userId });
-  userItem.value = data;
+  try {
+    const { data } = await getUerItem({ user_id: userId });
+    userItem.value = data;
+    itemVisible.value = true;
+  } catch {
+    console.log(111);
+  }
+};
+
+// 抽屉栏
+// 确定
+const handleOk = () => {
+  itemVisible.value = false;
+};
+// 取消
+const handleCancel = () => {
+  itemVisible.value = false;
 };
 </script>
 
 <template>
-  <a-table :columns="columns" :data="userInfo" stripe>
-    <template #headshot="{ record }">
-      <img :src="record.headshot" alt="" class="uer-headshot" />
-    </template>
-    <template #roles="{ record }">
-      {{ record.roles[0].role_name }}
-    </template>
-    <template #user_status="{ record }">
-      <a-switch
-        v-model="record.user_status"
-        :default-checked="record.user_status"
-        :checked-value="true"
-        :unchecked-value="false"
-        @change="switchChange(record.user_status)"
-      />
-      <span class="user-state">{{ record.user_status ? '封禁' : '正常' }}</span>
-    </template>
-    <template #optional="{ record }">
-      <div class="edit">
-        <a-button type="outline" size="mini" @click="editItem(record.id)">
-          编辑
-        </a-button>
-        <a-button type="outline" status="danger" size="mini">删除</a-button>
+  <div>
+    <!-- 以id为当前列的唯一标识 -->
+    <a-table
+      v-model:selectedKeys="selectedKeys"
+      :columns="columns"
+      :data="userInfo"
+      row-key="id"
+      stripe
+      :row-selection="rowSelection"
+      :pagination="pagination"
+    >
+      <template #headshot="{ record }">
+        <div>
+          <img :src="record.headshot" alt="" class="uer-headshot" />
+        </div>
+      </template>
+      <template #roles="{ record }">
+        <div>
+          {{ record.roles[0].role_name }}
+        </div>
+      </template>
+      <template #user_status="{ record }">
+        <div>
+          <a-switch
+            v-model="record.user_status"
+            :default-checked="record.user_status"
+            :checked-value="true"
+            :unchecked-value="false"
+            @change="switchChange(record.user_status)"
+          />
+          <span class="user-state">
+            {{ record.user_status ? '封禁' : '正常' }}
+          </span>
+        </div>
+      </template>
+      <template #optional="{ record }">
+        <div class="edit">
+          <span>
+            <a-button type="outline" size="mini" @click="editItem(record.id)">
+              编辑
+            </a-button>
+          </span>
+          <span>
+            <a-button type="outline" status="danger" size="mini">删除</a-button>
+          </span>
+        </div>
+      </template>
+    </a-table>
+    <a-drawer
+      :width="340"
+      :header="!itemCustom.includes('hide header')"
+      :footer="!itemCustom.includes('hide footer')"
+      :visible="itemVisible"
+      unmountOnClose
+      @ok="handleOk"
+      @cancel="handleCancel"
+    >
+      <template #title>Title</template>
+      <div>
+        You can customize modal body text by the current situation. This modal
+        will be closed immediately once you press the OK button.
       </div>
-    </template>
-  </a-table>
+    </a-drawer>
+  </div>
 </template>
 
 <style scoped>
@@ -117,6 +187,6 @@ const editItem = async userId => {
   margin-left: 12px;
 }
 .edit button {
-  margin: 5px 0;
+  margin: 0px 5px;
 }
 </style>
