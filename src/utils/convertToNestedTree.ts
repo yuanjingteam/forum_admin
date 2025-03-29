5d1e6c946ed508ebd13df7b535fd605d6504f936
hanshuobo <1432101420@qq.com>
2025-03-29 09:26:34 +0800

    feat: foreach null_3

--- src/utils/convertToNestedTree.ts
+++ src/utils/convertToNestedTree.ts
@@ -4,12 +4,12 @@ export function convertToNestedTree(data) {
   const result = [];
 
   // 初始化映射表
-  data.forEach(item => {
+  data?.forEach(item => {
     map[item.id] = { ...item }; // 将每个节点存储在映射表中
   });
 
   // 遍历数据，构建父子关系
-  data.forEach(item => {
+  data?.forEach(item => {
     if (item.pid === 0) {
       // 如果 pid 为 0，则为根节点
       result.push(map[item.id]);
